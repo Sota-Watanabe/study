e93107756606e99b4150069dbe6b06c29e55bc9a
Sota-Watanabe <cy16179@shibaura-it.ac.jp>
2019-12-13 17:56:37 +0900

    del cp

--- cmd/check_curl.sh
+++ cmd/check_curl.sh
@@ -4,11 +4,4 @@ if cluster/kubectl.sh get configmap config-istio -n knative-serving > /dev/null;
        INGRESSGATEWAY=istio-ingressgateway
 fi
 export IP_ADDRESS=localhost:$(cluster/kubectl.sh get svc $INGRESSGATEWAY --namespace istio-system   --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')
-curl -H "Host: $1.default.example.com" http://${IP_ADDRESS}  -o /dev/null -w "%{http_code}\n" -s
-
-while getopts "c" OPT
-do
-  case $OPT in
-    c) docker checkpoint create $(docker ps -f name=user -q --no-trunc) array-init --checkpoint-dir=/cp;;
-  esac
-done
+curl -H "Host: $1.default.example.com" http://${IP_ADDRESS}$2  -o /dev/null -w "%{http_code}\n" -s
