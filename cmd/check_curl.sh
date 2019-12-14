1292fd1245fb8bd72fdb03e032b227f8ba4042e0
Sota-Watanabe <cy16179@shibaura-it.ac.jp>
2019-12-14 17:20:42 +0900

    add time

--- cmd/check_curl.sh
+++ cmd/check_curl.sh
@@ -1,7 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 cd /home/watanabe/go/src/k8s.io/kubernetes-v1.15.5
 if cluster/kubectl.sh get configmap config-istio -n knative-serving > /dev/null; then
        INGRESSGATEWAY=istio-ingressgateway
 fi
 export IP_ADDRESS=localhost:$(cluster/kubectl.sh get svc $INGRESSGATEWAY --namespace istio-system   --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')
-curl -H "Host: $1.default.example.com" http://${IP_ADDRESS}$2  -o /dev/null -w "%{http_code}\n" -s
+start_time=$(date +"%s.%3N")
+echo looping ...
+while :
+do
+  stats=`curl -H "Host: $1.default.example.com" http://${IP_ADDRESS}$2  -o /dev/null -w "%{http_code}\n" -s`
+  if [ "$stats" = "200" ] ; then
+    # echo '200 OK'
+    break
+  fi
+done
+end_time=$(date +"%s.%3N")
+# time=$((end_time - start_time))
+
+echo "scale=1; $end_time - $start_time" | bc
+# while getopts "c" OPT
+# do
+#   case $OPT in
+#     c) docker checkpoint create $(docker ps -f name=user -q --no-trunc) array-init --checkpoint-dir=/cp;;
+#   esac
+# done
